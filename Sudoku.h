38928621ef0457fa2ebd8af622e0942dfbbfbb89
F74046103 <c09721123@gmail.com>
2016-04-03 21:49:26 +0800

    error

---
+++ Sudoku.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<ctime>
+#include <cstdlib>
+class Sudoku{
+	public:
+		   void giveQuestion();
+		      void readIn();
+			     void changeNum(int a,int b);
+				    void changeRow(int a,int b);
+					   void changeCol(int a,int b);
+					      void rotate(int n);
+						     void rotate2(int n);
+							    void rotate3(int n);
+								   void flip(int n);
+								      int isAvailable(int puzzle[][9], int row, int col, int num);
+									     int fillSudoku(int puzzle[][9], int row, int col, int num);
+										    int isSolvable1();
+											   int isSolvable2();
+											      void solve();
+												     int map[9][9];
+													    int map2[9][9];
+														   int ans1[9][9];
+														      int ans2[9][9];
+															     int i;
+																    int j;
+																	   int k;
+																	      int l;
+};
